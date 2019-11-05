aa9841f152a756dc701d8b4f6e4e6c4f6ccbfb04
Joao Melo <jopcmelo@gmail.com>
2019-11-05 16:16:50 -0300

    fix: Fix some deps and add actions, reducers and sagas exampes

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,7 +1,7 @@
 import { all } from 'redux-saga/effects';
 
+import user from './user/sagas';
+
 export default function* rootSaga() {
-  return yield all([
-    // sagas here
-  ]);
+  return yield all([user]);
 }
